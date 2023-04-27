6dbb981435009927bb20e04aefd2dbf9ffa07480
yuta24 <yuta24.225@gmail.com>
2023-04-27 22:56:01 +0900

    Set support platform to macOS v12

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "swift-analyzer",
+    platforms: [.macOS(.v12)],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
     ],
