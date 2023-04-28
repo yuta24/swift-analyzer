a1b5b834880a0c40a169b0803f1d10dd31ea76bb
yuta24 <yuta24.225@gmail.com>
2023-04-28 22:50:49 +0900

    Impl LineOfCode

--- Sources/swift-analyzer/LineOfCode.swift
+++ Sources/swift-analyzer/LineOfCode.swift
@@ -1,8 +1,31 @@
+import Foundation
 import ArgumentParser
 
 extension SwiftAnalyzer {
     struct LineOfCode: AsyncParsableCommand {
+        @Argument<String>
+        var path
+
         func run() async throws {
+            let url = URL(fileURLWithPath: path)
+            let content = try String(contentsOf: url)
+            let lines = content.components(separatedBy: .newlines)
+            let total = lines.count
+
+            let blank = lines.reduce(into: 0) { result, line in
+                result += line.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ? 1 : 0
+            }
+            // TODO: improve comment count
+            let comment = lines.reduce(into: 0) { result, line in
+                result += line.trimmingCharacters(in: .whitespacesAndNewlines).hasPrefix("//") ? 1 : 0
+            }
+
+            print("""
+            total: \(total)
+            blank: \(blank)
+            comment: \(comment)
+            code: \(total - blank - comment)
+            """)
         }
     }
 }
