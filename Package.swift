bc2d807d21466e1428398741cb262fff4a599481
yuta24 <yuta24.225@gmail.com>
2023-04-30 23:23:57 +0900

    Change platforms to macOS v13

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "swift-analyzer",
-    platforms: [.macOS(.v12)],
+    platforms: [.macOS(.v13)],
     products: [.executable(name: "swift-analyzer", targets: ["SwiftAnalyzer"])],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
